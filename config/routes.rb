e21269b31ebe6e2c0965f850448909b8729d0718
Eliza Upadhyaya <eliza.upadhyaya@gmail.com>
2023-02-07 18:36:46 +0000

    created models and seeded database

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,21 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+
+  # see all restos
+  root "restaurants#index"
+
+  # add a new resto and be redirected to the page of 1 new resto
+
+  get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+  post "restaurants", to: "restaurants#create"
+
+  # can see all the restos with all the associated reviews
+  # GET "restaurants/38" <- nested resource
+
+  # can add a new review to a restaurant
+  # GET "restaurants/38/reviews/new"
+  # POST "restaurants/38/reviews"
+
+
 end
