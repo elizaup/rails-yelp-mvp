3f06b1f70e2eda9b6f71d9ac75c3a611cce93357
Eliza Upadhyaya <eliza.upadhyaya@gmail.com>
2023-02-08 16:56:33 +0000

    completed all user stories

--- config/routes.rb
+++ config/routes.rb
@@ -4,19 +4,23 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
 
   # see all restos
-  root "restaurants#index"
+  get "restaurants", to: "restaurants#index"
 
   # add a new resto and be redirected to the page of 1 new resto
 
+  #
   get "restaurants/new", to: "restaurants#new", as: :new_restaurant
   post "restaurants", to: "restaurants#create"
 
-  # can see all the restos with all the associated reviews
+  # can see all the details of 1 resto, with all the associated reviews
   # GET "restaurants/38" <- nested resource
+
+  resources :restaurants, only: [:show] do
+    resources :reviews, only: [:show, :new]
+    # resources :reviews, only: [:new]
+  end
 
   # can add a new review to a restaurant
   # GET "restaurants/38/reviews/new"
   # POST "restaurants/38/reviews"
-
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,9 @@
 
 Restaurant.create([{ name: "Dishoom",  address: "London" , category:"chinese", phone_number:"5555"} ])
 Restaurant.create([{ name: "Pizza",  address: "Naples" ,  category:"italian", phone_number:"1234" }])
-Restaurant.create([{ name: "La Rouge",  address: "Brussels:" ,  category: "belgian", phone_number: "+352135239"} ])
-Restaurant.create([{ name: "Bella Napoli",  address: "Barcelona:" ,  category: "italian", phone_number: "+34538493"} ])
-Restaurant.create([{ name: "Takumi",  address: "Barcelona:" ,  category: "japanese", phone_number: "+34644291711"} ])
+Restaurant.create([{ name: "La Rouge",  address: "Brussels" ,  category: "belgian", phone_number: "+352135239"} ])
+Restaurant.create([{ name: "Bella Napoli",  address: "Barcelona" ,  category: "italian", phone_number: "+34538493"} ])
+Restaurant.create([{ name: "Takumi",  address: "Barcelona" ,  category: "japanese", phone_number: "+34644291711"} ])
+
+Review.create([{ rating: 5, content: "delicious pizza, in the heart of naples. friendly service. got a free limoncello", restaurant_id: 6}])
+Review.create([{ rating: 5, content: "delicious pizza and pasta and great wine.", restaurant_id: 6}])
